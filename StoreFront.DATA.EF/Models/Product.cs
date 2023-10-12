d239ef78025bf9e82e8b694373968d724694289f
Alex Davenport <alexd8052@gmail.com>
2023-10-11 21:25:08 -0500

    Implemented entity framework

---
+++ StoreFront.DATA.EF/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.DATA.EF.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderProducts = new HashSet<OrderProduct>();
+            ProductPlatforms = new HashSet<ProductPlatform>();
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal ProductPrice { get; set; }
+        public string ProductDescription { get; set; } = null!;
+        public string? ProductImage { get; set; }
+        public int CategoryId { get; set; }
+        public int? ManufacturerId { get; set; }
+        public int? ProductStatus { get; set; }
+
+        public virtual Category Category { get; set; } = null!;
+        public virtual Manufacturer? Manufacturer { get; set; }
+        public virtual ProductStatus? ProductStatusNavigation { get; set; }
+        public virtual ICollection<OrderProduct> OrderProducts { get; set; }
+        public virtual ICollection<ProductPlatform> ProductPlatforms { get; set; }
+    }
+}
